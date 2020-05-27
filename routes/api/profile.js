5b1b412e1c66842cf5e76f85b74f2ba92e364941
imuraki <akhilkc9@gmail.com>
2020-05-27 19:16:30 -0400

    Config and profile.js changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,8 @@ const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const request = require("request");
+const config = require("config");
 
 router.get("/me", auth, async (req, res) => {
   try {
@@ -176,6 +178,92 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.log(err.message);
+    res.send("Internal server error");
+  }
+});
+
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is required").not().isEmpty(),
+      check("degree", "Degree is required").not().isEmpty(),
+      check("fieldofstudy", "Field of Study is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const education = _.pick(req.body, [
+      "school",
+      "degree",
+      "fieldofstudy",
+      "from",
+      "to",
+      "current",
+      "description",
+    ]);
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(education);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.send("Internal server error");
+    }
+  }
+);
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //Get Remove index
+    const removeindex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.id);
+
+    profile.education.splice(removeindex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.send("Internal server error");
+  }
+});
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc
+      &client_id=${config.get("githubClientId")}&client_secret=${config.get(
+        "githubSecret"
+      )}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode != 200) {
+        return res.status(404).json({ msg: "No Github Profile Found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
     res.send("Internal server error");
   }
 });
